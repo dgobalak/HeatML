d03362cf99f75fa5e629b251e696e99fc40ede18
dgobalak <dgobalak@uwaterloo.ca>
2021-10-03 17:54:14 -0400

    Converted web app to desktop app with flaskwebgui

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from src import app
-# from flaskwebgui import FlaskUI
+from flaskwebgui import FlaskUI
 
 # To convert to gui: make sure to set dubug to False and uncomment JS script
 
 if __name__ == "__main__":
-    app.run()
-    # FlaskUI(app, width=1600, height=1000, maximized=True).run()
+    # app.run()
+    FlaskUI(app, width=1600, height=1000, maximized=True).run()
